422410b65eded29f772caada8fbe9a1db1319f60
lagger0k <max9119max@gmail.com>
2023-06-12 16:14:28 +0300

    feat: create base etl structure

--- etl/config/settings.py
+++ etl/config/settings.py
@@ -7,10 +7,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Settings(BaseSettings):
+    """
+    Настройки ETL сервиса.
+    """
+
+    # DB
     DB_HOST: str = Field(..., env='DB_HOST')
     DB_PORT: str = Field(..., env='DB_PORT')
     DB_USER: str = Field(..., env='DB_USER')
     DB_PASSWORD: str = Field(..., env='DB_PASSWORD')
+    # Extractor
+    REQUEST_LIMIT: int = Field(..., env='REQUEST_LIMIT')
 
     class Config:
         env_file = '.env'
